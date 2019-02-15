a3d7469b471f96fba7e4d63045ce2e1745a8a27f
wongzl <leezhi131@gmail.com>
2019-02-15 18:40:01 +0800

    初探springboot

---
+++ hello-spring/src/main/java/com/wongzl/spring/hellospring/HelloSpringApplication.java
@@ -0,0 +1,25 @@
+package com.wongzl.spring.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wongzl
+ */
+@RestController
+@SpringBootApplication
+public class HelloSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
+
